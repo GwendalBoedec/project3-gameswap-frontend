f11a404eb585c0024b047d51f65c8d66ad911bff
Gwendal Boedec <boedec.g@gmail.com>
2025-03-11 14:57:00 +0100

    add react select to improve ux in multiple selection forms

---
+++ src/config/FORMS_OPTIONS.js
@@ -0,0 +1,14 @@
+const consoleOptions = [
+    { value: "gameboy", label: "Gameboy" },
+    { value: "gameboy advanced", label: "Gameboy Advanced" },
+    { value: "gamecube", label: "Gamecube" },
+    { value: "NES", label: "NES" },
+    { value: "nintendo 64", label: "Nintendo 64" },
+    { value: "super nintendo", label: "Super Nintendo" },
+    { value: "playstation 1", label: "Playstation 1" },
+    { value: "playstation 2", label: "Playstation 2" },
+    { value: "PSP", label: "PSP" },
+    { value: "Xbox", label: "Xbox" },
+];
+
+export default consoleOptions
--- src/pages/SignupPage.jsx
+++ src/pages/SignupPage.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import API_URL from "../config/API_URL";
+import consoleOptions from "../config/FORMS_OPTIONS";
+import Select from "react-select";
 
 
 function SignupPage(props) {
@@ -19,90 +21,85 @@ function SignupPage(props) {
     const handlePassword = (e) => setPassword(e.target.value);
     const handleUsername = (e) => setUsername(e.target.value);
     const handleCity = (e) => setCity(e.target.value);
-    const handleFavoriteConsoles = (e) => setFavoriteConsoles(Array.from(e.target.selectedOptions, option => option.value));
+    const handleFavoriteConsoles = (selectedOptions) => setFavoriteConsoles(selectedOptions || []);
     const handleFavoriteGameStyles = (e) => setFavoriteGameStyles(Array.from(e.target.selectedOptions, option => option.value));
 
     const handleSignupSubmit = (e) => {
         e.preventDefault();
-         // Create an object representing the request body
-         const requestBody = { email, password, username, city, favoriteConsoles, favoriteGameStyles };
+        // Create an object representing the request body
+        const requestBody = {
+            email,
+            password,
+            username,
+            city,
+            favoriteConsoles: favoriteConsoles.map(option => option.value),
+            favoriteGameStyles
+        };
 
         // Make an axios request to the API
-    // If the POST request is a successful redirect to the login page
-    // If the request resolves with an error, set the error message in the state
+        // If the POST request is a successful redirect to the login page
+        // If the request resolves with an error, set the error message in the state
         axios.post(`${API_URL}/auth/signup`, requestBody)
-        .then((response) => {
-            navigate("/login")
-        })
-        .catch((error) => {
-            const errorDescription = error.response?.data?.message || "an error occured";
-            setErrorMessage(errorDescription);
-        })
+            .then((response) => {
+                navigate("/login")
+            })
+            .catch((error) => {
+                const errorDescription = error.response?.data?.message || "an error occured";
+                setErrorMessage(errorDescription);
+            })
     };
 
     return (
         <div className="">
             <h1>Sign Up</h1>
-    
-            <form onSubmit={handleSignupSubmit}>
-            <label>Email:</label>
-            <input 
-                type="email"
-                name="email"
-                value={email}
-                onChange={handleEmail}
-            />
-    
-            <label>Password:</label>
-            <input 
-                type="password"
-                name="password"
-                value={password}
-                onChange={handlePassword}
-            />
-    
-            <label>username:</label>
-            <input 
-                type="text"
-                name="username"
-                value={username}
-                onChange={handleUsername}
-            />
 
-            <label>City:</label>
-                <input 
+            <form onSubmit={handleSignupSubmit}>
+                <label>Email:</label>
+                <input
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={handleEmail}
+                />
+
+                <label>Password:</label>
+                <input
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={handlePassword}
+                />
+
+                <label>username:</label>
+                <input
+                    type="text"
+                    name="username"
+                    value={username}
+                    onChange={handleUsername}
+                />
+
+                <label>City:</label>
+                <input
                     type="text"
                     name="city"
                     value={city}
                     onChange={handleCity}
-            />
+                />
 
-            <label>Favorite Console(s):</label>
-                <select 
-                    
-                    name="favoriteConsoles"
-                    value={favoriteConsoles || []}
-                    onChange={handleFavoriteConsoles}
-                    multiple>
-                    <option value="" disabled> select your favorite console </option>
-                    <option value="gameboy"> Gameboy </option>
-                    <option value="gameboy advanced"> Gameboy advanced </option>
-                    <option value="gamecube"> Gamecube </option>
-                    <option value="NES"> NES </option>
-                    <option value="nintendo 64"> Nintendo 64 </option>
-                    <option value="super nintendo"> Super Nintendo </option>
-                    <option value="playstation 1"> Playstation 1 </option>
-                    <option value="playstation 2"> Playstation 2 </option>
-                    <option value="PSP"> PSP </option>
-                    <option value="Xbox"> Xbox </option>
-                </select>
+                <label>Favorite Console(s):</label>
+                <Select
+                    options={consoleOptions}
+                    isMulti
+                    value={favoriteConsoles}
+                    onChange={handleFavoriteConsoles} />
+
 
                 <label>Favorite Game Style(s):</label>
-                <select 
-                    
+                <select
+
                     name="favoriteGameStyles"
                     value={favoriteGameStyles || []}
-                    onChange= {handleFavoriteGameStyles}
+                    onChange={handleFavoriteGameStyles}
                     multiple>
                     <option value="" disabled> select your favorite game style </option>
                     <option value="adventure"> Adventure </option>
@@ -113,15 +110,15 @@ function SignupPage(props) {
                     <option value="RPG"> RPG </option>
                     <option value="strategy"> Strategy </option>
                 </select>
-    
-            <button type="submit">Sign Up</button>
+
+                <button type="submit">Sign Up</button>
             </form>
-            { errorMessage && <p className="error-message">{errorMessage}</p> }
-    
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+
             <p>Already have account?</p>
             <Link to={"/login"}> Login</Link>
         </div>
-)
+    )
 }
 
 export default SignupPage;
