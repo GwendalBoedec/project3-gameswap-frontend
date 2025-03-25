9ddafb41ef8344ed2ed4f8e43f90713f1602061d
Gwendal Boedec <boedec.g@gmail.com>
2025-03-25 15:00:36 +0100

    quick clean on myprofile page

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
 import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 import "../styles/myprofile.css";
 import { Button } from "@mantine/core";
 import Loader from "../components/Loader";
